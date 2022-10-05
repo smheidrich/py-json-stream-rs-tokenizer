ff2df399bc44aa106f0307ddf263d26b47619055
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2022-10-06 01:19:18 +0200

    Add requires_bigint to rust_tokenizer_or_raise (#31)
    * Add requires_bigint to rust_tokenizer_or_raise
    
    * Bump version to 0.4.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="json-stream-rs-tokenizer",
-    version="0.4.6",
+    version="0.4.7",
     rust_extensions=[
         RustExtension(
             "json_stream_rs_tokenizer.json_stream_rs_tokenizer",
