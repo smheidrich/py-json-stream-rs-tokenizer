c16c594fc8f909f36e3f08e185bc852e116ffd72
smheidrich <smheidrich@weltenfunktion.de>
2022-10-16 00:24:58 +0200

    Use json-stream-to-standard-types for benchmarks
    Removes the previous dependency on a specific PR branch of json-stream
    introducing the same functionality.

--- json_stream_rs_tokenizer/benchmark/app.py
+++ json_stream_rs_tokenizer/benchmark/app.py
@@ -5,18 +5,12 @@ from tempfile import TemporaryDirectory
 
 import json_stream as js
 from contexttimer import Timer
+from json_stream_to_standard_types import to_standard_types
 from tqdm import tqdm
 
 import json_stream_rs_tokenizer as jsrs
 
 from .random_json_generator import RandomJsonGenerator
-
-try:
-    js.to_standard_types
-except AttributeError as e:
-    raise ImportError(
-        "benchmarks currently require a patched version of json-stream"
-    ) from e
 
 
 def shuffled(l):
@@ -48,9 +42,7 @@ def main(json_bytes=2e6):
             with Timer() as t:
                 with random_json_file_path.open() as f:
                     l = load_fn(f)
-                    parsed = [
-                        js.to_standard_types(x) for x in tqdm(l, total=100)
-                    ]
+                    parsed = [to_standard_types(x) for x in tqdm(l, total=100)]
             print(f"{tokenizer_type} time: {t.elapsed:.2f} s")
             results[tokenizer_type]["elapsed"] = t.elapsed
             results[tokenizer_type]["parsed"] = parsed
