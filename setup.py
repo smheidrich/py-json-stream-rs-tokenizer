2136fed0b88e8e921fdb2e8e20e793a27124d4a8
smheidrich <smheidrich@weltenfunktion.de>
2022-10-15 21:25:10 +0200

    Lower minimum Python version to 3.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
             "https://github.com/smheidrich/py-json-stream-rs-tokenizer"
         )
     },
-    python_requires=">=3.7,<4",
+    python_requires=">=3.5,<4",
     install_requires=[],
     extras_require={
         "benchmark": [
@@ -44,6 +44,8 @@ setup(
         "Programming Language :: Rust",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
