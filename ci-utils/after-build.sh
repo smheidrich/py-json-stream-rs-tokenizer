5e7084aa10cef59196efe894bef3ab00fe9e62be
smheidrich <smheidrich@weltenfunktion.de>
2022-10-22 22:01:02 +0200

    Remove target dir before copying from cache

--- ci-utils/after-build.sh
+++ ci-utils/after-build.sh
@@ -2,6 +2,6 @@
 ver=$( \
   python3 -c \
   'from platform import *; print("-".join([python_implementation(), python_version(), platform()]))' \
-);
-mkdir -p "$HOST_HOME_DIR/cargo-target-dirs/$ver";
+)
+mkdir -p "$HOST_HOME_DIR/cargo-target-dirs/$ver"
 mv "$PROJ_DIR"/target "$HOST_HOME_DIR/cargo-target-dirs/$ver"
--- ci-utils/before-build.sh
+++ ci-utils/before-build.sh
@@ -2,5 +2,6 @@
 ver=$( \
   python3 -c \
   'from platform import *; print("-".join([python_implementation(), python_version(), platform()]))' \
-);
+)
+rm -rf "$PROJ_DIR/target"
 mv "$HOST_HOME_DIR/cargo-target-dirs/$ver" "$PROJ_DIR/target" || true
