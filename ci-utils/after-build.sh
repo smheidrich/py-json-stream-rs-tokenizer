d1a5f499d4ab83609ce9162e09646cf83db9d2ac
smheidrich <smheidrich@weltenfunktion.de>
2022-10-22 22:08:47 +0200

    Remove ver-spec. cache dir before caching

--- ci-utils/after-build.sh
+++ ci-utils/after-build.sh
@@ -4,4 +4,5 @@ ver=$( \
   'from platform import *; print("-".join([python_implementation(), python_version(), platform()]))' \
 )
 mkdir -p "$HOST_HOME_DIR/cargo-target-dirs/$ver"
+rm -rf "$HOST_HOME_DIR/cargo-target-dirs/$ver" || true
 mv "$PROJ_DIR"/target "$HOST_HOME_DIR/cargo-target-dirs/$ver"
