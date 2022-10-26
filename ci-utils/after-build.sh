9d58b9f21f0792ec8c689aab7621edb2592a24a6
smheidrich <smheidrich@weltenfunktion.de>
2022-10-27 01:16:22 +0200

    Use rsync for after-build cache storage

--- ci-utils/after-build.sh
+++ ci-utils/after-build.sh
@@ -12,6 +12,6 @@ mv "$CARGO_TARGET_DIR" "$HOST_HOME_DIR/cargo-target-dirs/$ver/"
 # save cargo home cache (will happen more than once on linux but that's fine)
 ver=$(cat "$HOST_HOME_DIR/outer-ver")
 [ -n "$ver" ] || { echo "error loading outer version"; exit 1; }
-rm -rf "$HOST_HOME_DIR/cargo-home-dirs/$ver"
+#rm -rf "$HOST_HOME_DIR/cargo-home-dirs/$ver"
 mkdir -p "$HOST_HOME_DIR/cargo-home-dirs/$ver"
-cp -pR "$CARGO_HOME" "$RUSTUP_HOME" "$HOST_HOME_DIR/cargo-home-dirs/$ver/"
+rsync -au "$CARGO_HOME" "$RUSTUP_HOME" "$HOST_HOME_DIR/cargo-home-dirs/$ver/"
