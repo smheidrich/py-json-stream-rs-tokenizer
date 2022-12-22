718c24693c3ec8d69b88dfac7ed1f87281a5aae9
smheidrich <smheidrich@weltenfunktion.de>
2022-12-22 03:11:41 +0100

    Add unseekable text stream support (1 char aat)

--- src/suitable_stream.rs
+++ src/suitable_stream.rs
@@ -3,6 +3,7 @@ use crate::py_bytes_stream::PyBytesStream;
 use crate::py_text_stream::PyTextStream;
 use crate::suitable_seekable_bytes_stream::SuitableSeekableBytesStream;
 use crate::suitable_seekable_text_stream::SuitableSeekableTextStream;
+use crate::suitable_unseekable_text_stream::SuitableUnseekableTextStream;
 use pyo3::exceptions::PyTypeError;
 use pyo3::types::{PyBytes, PyString};
 use pyo3::{PyObject, PyResult, Python};
@@ -24,10 +25,21 @@ pub fn make_suitable_stream(stream: PyObject) -> PyResult<Box<dyn ParkCursorChar
             Ok(ReadReturnType::Other(format!("{}", read_result.get_type())))
         }
     })?;
+    let seekable: bool = Python::with_gil(|py| -> PyResult<bool> {
+        Ok(stream
+            .as_ref(py)
+            .call_method1("seekable", ())?
+            .extract::<bool>()?)
+    })?;
     match read_return_type {
-        ReadReturnType::String => Ok(Box::new(SuitableSeekableTextStream::new(
-            PyTextStream::new(stream),
-        ))),
+        ReadReturnType::String => {
+            let py_text_stream = PyTextStream::new(stream);
+            Ok(if seekable {
+                Box::new(SuitableSeekableTextStream::new(py_text_stream))
+            } else {
+                Box::new(SuitableUnseekableTextStream::new(py_text_stream))
+            })
+        }
         ReadReturnType::Bytes => Ok(Box::new(SuitableSeekableBytesStream::new(
             PyBytesStream::new(stream),
         ))),
