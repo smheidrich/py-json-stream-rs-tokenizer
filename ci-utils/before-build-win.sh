03666d75734e1ffb3e2069e4df4904af696bbaf1
smheidrich <smheidrich@weltenfunktion.de>
2023-06-18 14:37:19 +0200

    Fix moving cargo/rustup dirs after build

--- ci-utils/before-build-win.sh
+++ ci-utils/before-build-win.sh
@@ -4,13 +4,13 @@
 ver=$( \
   python3 -c \
   'from platform import *; print("-".join([platform()]))' \
-  | tee "$HOST_HOME_DIR/outer-ver" \
+  | tee "$HOST_HOME_DIR_UNIX/outer-ver" \
 )
-rm -rf "$CARGO_HOME"
-rm -rf "$RUSTUP_HOME"
-mv "$HOST_HOME_DIR/cargo-home-dirs/$ver/.cargo" \
-  "$HOST_HOME_DIR/cargo-home-dirs/$ver/.rustup" \
-  "$HOST_HOME_DIR/" || true
+rm -rf "$CARGO_HOME_UNIX"
+rm -rf "$RUSTUP_HOME_UNIX"
+mv "$HOST_HOME_DIR_UNIX/cargo-home-dirs/$ver/.cargo" \
+  "$HOST_HOME_DIR_UNIX/cargo-home-dirs/$ver/.rustup" \
+  "$HOST_HOME_DIR_UNIX/" || true
 
 # check if cargo avail and download if not
 if ! cargo -V; then
@@ -25,6 +25,6 @@ ver=$( \
   python3 -c \
   'from platform import *; print("-".join([python_implementation(), python_version(), platform()]))' \
 )
-rm -rf "$HOST_HOME_DIR/target"
-mv "$HOST_HOME_DIR/cargo-target-dirs/$ver/target" "$HOST_HOME_DIR/" \
+rm -rf "$HOST_HOME_DIR_UNIX/target"
+mv "$HOST_HOME_DIR_UNIX/cargo-target-dirs/$ver/target" "$HOST_HOME_DIR_UNIX/" \
 || echo "Could not restore Cargo target dir from cache"
