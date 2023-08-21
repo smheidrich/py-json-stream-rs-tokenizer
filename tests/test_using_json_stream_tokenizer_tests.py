4c8cb3af7956842e3180c72d85baec7b06e5cf39
smheidrich <smheidrich@weltenfunktion.de>
2023-08-21 21:29:46 +0200

    Adjust json-stream test module paths

--- tests/test_using_json_stream_tokenizer_tests.py
+++ tests/test_using_json_stream_tokenizer_tests.py
@@ -5,14 +5,16 @@ from unittest.mock import patch
 
 import pytest
 
-from json_stream.tests.test_tokenizer import TestJsonTokenization
+from json_stream.tokenizer.tests.test_tokenizer import TestJsonTokenization
 from json_stream.tests.test_buffering import TestBuffering
 from json_stream_rs_tokenizer import RustTokenizer
 
 
 @pytest.fixture(autouse=True, scope="module")
 def override_tokenizer():
-    with patch("json_stream.tests.test_tokenizer.tokenize", RustTokenizer):
+    with patch(
+        "json_stream.tokenizer.tests.test_tokenizer.tokenize", RustTokenizer
+    ), patch("json_stream.tests.test_buffering.tokenize", RustTokenizer):
         yield
 
 
