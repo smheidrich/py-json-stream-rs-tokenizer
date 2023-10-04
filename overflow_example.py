13c1e69321fe4c6cd1a38e8e80131d4f48c0000f
smheidrich <smheidrich@weltenfunktion.de>
2023-10-04 19:43:46 +0200

    Demonstrate overflow with faster (?) example

--- overflow_example.py
+++ overflow_example.py
@@ -1,97 +1,47 @@
-# from: https://github.com/smheidrich/py-json-stream-rs-tokenizer/issues/93#issuecomment-1744756519
-import json
-import json_stream
-from json_stream import streamable_list, streamable_dict
-from random import choice
-from string import ascii_lowercase
-from tqdm import tqdm
+from json_stream_rs_tokenizer import RustTokenizer
 
+class InfiniteJsonFile:
+  start = "["
+  elem = "12345, "
 
-def main():
-    print("Generating ...")
-    with open("foo.json", "w") as f:
-        json.dump(
-            obj=streamable_list(
-                tqdm(
-                    iterable=random_json(
-                        n_lists=100,
-                        n_dicts=100,
-                        n_items_per_dict=100,
-                    ),
-                    total=100,
-                )
-            ),
-            fp=f,
-            indent=4,
-        )
+  def __init__(self):
+    self.remainder = self.start
+    self.size = 0
 
-    print("Reading ...")
-    with open("foo.json", "r") as f:
-        foo = json_stream.load(f)
-        for bars in foo:
-            for bar in bars:
-                for k, v in bar.items():
-                    print(k, v)
+  def read(self, size):
+    if size == 0:
+      return self.start[:0]
+    s = self.remainder + self.elem * (size // len(self.elem) + 1)
+    self.remainder = s[size:]
+    s = s[:size]
+    self.size += len(s)
+    return s
 
-    print("Done.")
+  def seek(self, offset, whence):
+    match whence:
+      case 0:
+        if offset == 0:
+          self.remainder = self.start
+        else:
+          self.remainder = self.elem[(offset-1) % len(self.elem):]
+        self.size = offset
+      case 1:
+        assert offset == 0
+      case 2:
+        raise NotImplementedError("seek from end not impl")
+    return self.size
 
+  def seekable(self):
+    return True
 
-def random_json(
-    *,
-    n_lists: int,
-    n_dicts: int,
-    n_items_per_dict: int,
-):
-    for _ in range(n_lists):
-        yield streamable_list(
-            random_dicts(n_dicts=n_dicts, n_items_per_dict=n_items_per_dict)
-        )
+# f = InfiniteJsonFile()
+# for x in range(60):
+  # print(f.read(8), end="")
 
-
-def random_dicts(
-    *,
-    n_dicts: int,
-    n_items_per_dict: int,
-):
-    for _ in range(n_dicts):
-        yield streamable_dict(random_dict_items(n_items_per_dict=n_items_per_dict))
-
-
-def random_dict_items(
-    *,
-    n_items_per_dict: int,
-):
-    for _ in range(n_items_per_dict):
-        yield random_string(), random_value()
-
-
-def random_value():
-    return choice(
-        [
-            random_int,
-            random_float,
-            random_string,
-            random_bool,
-            lambda: None,
-        ]
-    )()
-
-
-def random_string():
-    return "".join(choice(ascii_lowercase) for _ in range(10))
-
-
-def random_int():
-    return choice([-1, 0, 1]) * choice(range(100))
-
-
-def random_float():
-    return choice([-1, 1]) * choice(range(100)) / 10
-
-
-def random_bool():
-    return choice([True, False])
-
-
-if __name__ == "__main__":
-    main()
+f = InfiniteJsonFile()
+r = RustTokenizer(f)
+N = 1000000000
+for i in range(N+1):
+  if i % (N/10) == 0:
+    print(f"{i}/{N} ({f.size} bytes)")
+  next(r)
