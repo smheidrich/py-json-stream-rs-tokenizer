45a4fb9ac56562af66f88006de612fd6c3912d45
smheidrich <smheidrich@weltenfunktion.de>
2023-10-06 02:19:07 +0200

    Cover large (2^64+) cursor positions in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,27 @@ from io import BytesIO, StringIO
 import pytest
 
 
-@pytest.fixture(params=["str", "bytes", "str-unseekable", "bytes-unseekable"])
+class StringIOWithLargeCursorPositions(StringIO):
+    """
+    Simulates very large (> 2^64) cursor positions as occur on Windows.
+    """
+
+    def seek(self, offset, whence):
+        return super().seek(offset, whence) + 2**64
+
+    def tell(self):
+        return super().tell() + 2**64
+
+
+@pytest.fixture(
+    params=[
+        "str",
+        "bytes",
+        "str-unseekable",
+        "bytes-unseekable",
+        "str-largecursorpos",
+    ]
+)
 def to_bytes_or_str_buf(request):
     if request.param == "str":
         return lambda s: StringIO(s)
@@ -15,6 +35,8 @@ def to_bytes_or_str_buf(request):
             return sio
 
         return make_unseekable_stringio
+    elif request.param == "str-largecursorpos":
+        return lambda s: StringIOWithLargeCursorPositions(s)
     elif request.param == "bytes":
         return lambda s: BytesIO(s.encode("utf-8"))
     elif request.param == "bytes-unseekable":
