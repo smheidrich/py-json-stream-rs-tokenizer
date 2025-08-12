8f2f928342cdfa64a51a7663e54be233f0a71548
smheidrich <smheidrich@weltenfunktion.de>
2025-08-13 00:42:50 +0200

    Bump version to 0.4.30

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="json-stream-rs-tokenizer",
-    version="0.4.29",
+    version="0.4.30",
     rust_extensions=[
         RustExtension(
             "json_stream_rs_tokenizer.json_stream_rs_tokenizer",
